48b815dac4b9b7d580170218399719711eab41cb
AngryRacoon <70721854+electretboy@users.noreply.github.com>
2023-03-14 21:28:47 +0100

    update

--- YouTake.py
+++ YouTake.py
@@ -1,8 +1,9 @@
 
 from pytube import YouTube
+from pytube.cli import on_progress
 # Enter the YouTube video URL
-url = input("Enter the YouTube video link: ")
-yt = YouTube(url)
+url = input("Enter the YouTube video link (URL): ")
+yt = YouTube(url, on_progress_callback=on_progress)
 # Get the first stream with the highest resolution
 stream = yt.streams.get_highest_resolution()
 # Download the video to the current working directory
