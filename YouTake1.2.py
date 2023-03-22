eb499fd0796c22c39da7dc6075fb3bb288932fe5
AngryRacoon <70721854+electretboy@users.noreply.github.com>
2023-03-22 22:43:15 +0100

    update variables names

--- YouTake1.2.py
+++ YouTake1.2.py
@@ -4,21 +4,21 @@ import tkinter as tk
 from moviepy.editor import *
 
 def print_selection():
-    print(var1.get())
-    print(var2.get())
-    print(var3.get())
+    print(vid_qual.get())
+    print(vid_form.get())
+    print(aud_qual.get())
     print(entry.get())
     # Retrieve the value entered by the user in the entry widget
     url = entry.get()
     yt = YouTube(url, on_progress_callback=on_progress)
     # Get the stream with the resolution required by the user
-    stream = yt.streams.filter(res=var1.get(), file_extension="mp4").first()
+    stream = yt.streams.filter(res=vid_qual.get(), file_extension="mp4").first()
     # Download the video to the current working directory
     stream.download()
     print("Video downloaded successfully !")
-    if var2.get() != "mp4":
+    if vid_form.get() != "mp4":
         clip = VideoFileClip(stream)
-        output_file = f"{stream.split('.')[0]}.{var2.get()}"
+        output_file = f"{stream.split('.')[0]}.{vid_form.get()}"
         clip.write_videofile(output_file)
 
 video_quality = ["2160p", "1440p", "1080p", "720p", "480p", "360p", "240p", "144p"]
@@ -33,13 +33,13 @@ entry = tk.Entry(input_frame,width=75)
 entry.pack()
 input_frame.pack(padx=10, pady=10)
 
-var1 = tk.StringVar(value="Video quality")
-var2 = tk.StringVar(value="Video format")
-var3 = tk.StringVar(value="Audio format")
+vid_qual = tk.StringVar(value="Video quality")
+vid_form = tk.StringVar(value="Video format")
+aud_qual = tk.StringVar(value="Audio format")
 
-menu_video_quality = tk.OptionMenu(root, var1, *video_quality).pack(padx=10, pady=10)
-menu_video_format = tk.OptionMenu(root, var2, *video_format).pack()
-menu_audio_format = tk.OptionMenu(root, var3, *audio_format).pack(padx=10, pady=10)
+menu_video_quality = tk.OptionMenu(root, vid_qual, *video_quality).pack(padx=10, pady=10)
+menu_video_format = tk.OptionMenu(root, vid_form, *video_format).pack()
+menu_audio_format = tk.OptionMenu(root, aud_qual, *audio_format).pack(padx=10, pady=10)
 
 button = tk.Button(root, text="Download", command=print_selection, bg="green", bd=5).pack(side="bottom", padx=10, pady=10)
 
